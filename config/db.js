44f22bd15bafd1eceb83bc56a7bbd5b9b4909aaa
Arlyn0603 <162391993+Arlyn0603@users.noreply.github.com>
2025-04-03 23:44:14 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,16 +2,15 @@ const sql = require("mssql");
 
 
 const config = {
-    user: "Anita", // Reemplázalo con tu usuario de SQL Server
-    password: "Fgjdal123.", // Reemplázalo con tu contraseña
-    server: "ANAPC", // Reemplázalo con la dirección IP de tu servidor SQL Server
+    user: "Arlynn", // Reemplázalo con tu usuario de SQL Server
+    password: "Arlinms1.", // Reemplázalo con tu contraseña
+    server: "ARLYN", // Reemplázalo con la dirección IP de tu servidor SQL Server
     database: "EventosDB",
     options: {
         encrypt: false, // Desactiva en local, pero actívalo en producción
         trustServerCertificate: true // Solo si usas autenticación de Windows
     }
 };
-
 const poolPromise = new sql.ConnectionPool(config)
     .connect()
     .then(pool => {
